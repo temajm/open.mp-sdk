cc09c007330e1d514ba376f66011ef0a74147573
Y_Less <git-athena@y-less.com>
2022-04-01 18:40:29 +0100

    Add the fixes component.

---
+++ include/Server/Components/Fixes/fixes.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <sdk.hpp>
+
+static const UID FixesData_UID = UID(0x672d5d6fbb094ef7);
+struct IPlayerFixesData : public IExtension
+{
+    PROVIDE_EXT_UID(FixesData_UID);
+
+    /// Hide the player seen this library?
+    virtual bool seenLibrary(StringView animationLibrary) = 0;
+};
+
+static const UID FixessComponent_UID = UID(0xb5c615eff0329ff7);
+struct IFixessComponent : public IComponent
+{
+    PROVIDE_UID(FixessComponent_UID);
+};
+
