404004b5fce608dc91c543edc3740d7dcb53451f
Y_Less <git-athena@y-less.com>
2022-04-02 16:16:12 +0100

    Fix animations by preloading them instead.

--- include/Server/Components/Fixes/fixes.hpp
+++ include/Server/Components/Fixes/fixes.hpp
@@ -6,18 +6,11 @@ static const UID FixesData_UID = UID(0x672d5d6fbb094ef7);
 struct IPlayerFixesData : public IExtension
 {
     PROVIDE_EXT_UID(FixesData_UID);
-
-    /// Has the player seen this library?
-    virtual bool seenLibrary(StringView animationLibrary) = 0;
 };
 
 static const UID FixesComponent_UID = UID(0xb5c615eff0329ff7);
 struct IFixesComponent : public IComponent
 {
     PROVIDE_UID(FixesComponent_UID);
-
-	virtual void applyAnimation(IPlayer & player, AnimationData const & anim, PlayerAnimationSyncType sync) = 0;
-
-	virtual void clearAnimations(IPlayer & player, PlayerAnimationSyncType sync) = 0;
 };
 
