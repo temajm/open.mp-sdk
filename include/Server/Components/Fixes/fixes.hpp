dd6db2369db61d4f90a347e96f2b2cad11e143ab
Y_Less <git-athena@y-less.com>
2022-04-02 16:41:39 +0100

    Fix player paying for hospitals.

--- include/Server/Components/Fixes/fixes.hpp
+++ include/Server/Components/Fixes/fixes.hpp
@@ -6,11 +6,16 @@ static const UID FixesData_UID = UID(0x672d5d6fbb094ef7);
 struct IPlayerFixesData : public IExtension
 {
     PROVIDE_EXT_UID(FixesData_UID);
+
+	virtual int getLastCash() const = 0;
+	virtual void setLastCash(int cash) = 0;
 };
 
 static const UID FixesComponent_UID = UID(0xb5c615eff0329ff7);
 struct IFixesComponent : public IComponent
 {
     PROVIDE_UID(FixesComponent_UID);
+
+	virtual void spawnPlayer(IPlayer & player) = 0;
 };
 
