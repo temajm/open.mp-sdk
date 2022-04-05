08180c67317ff112364623816d365e46cc4ae351
Y_Less <git-athena@y-less.com>
2022-04-06 00:32:53 +0100

    Revert "Fix animations by preloading them instead."
    This reverts commit 404004b5fce608dc91c543edc3740d7dcb53451f.

--- include/Server/Components/Fixes/fixes.hpp
+++ include/Server/Components/Fixes/fixes.hpp
@@ -7,6 +7,9 @@ struct IPlayerFixesData : public IExtension
 {
     PROVIDE_EXT_UID(FixesData_UID);
 
+    /// Has the player seen this library?
+    virtual bool seenLibrary(StringView animationLibrary) = 0;
+
 	virtual int getLastCash() const = 0;
 	virtual void setLastCash(int cash) = 0;
 };
@@ -15,5 +18,9 @@ static const UID FixesComponent_UID = UID(0xb5c615eff0329ff7);
 struct IFixesComponent : public IComponent
 {
     PROVIDE_UID(FixesComponent_UID);
+
+	virtual void applyAnimation(IPlayer & player, AnimationData const & anim, PlayerAnimationSyncType sync) = 0;
+
+	virtual void clearAnimations(IPlayer & player, PlayerAnimationSyncType sync) = 0;
 };
 
