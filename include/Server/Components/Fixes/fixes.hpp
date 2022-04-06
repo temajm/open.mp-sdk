544c395e361d880c043547ccf4e9050ad9693d99
Y_Less <git-athena@y-less.com>
2022-04-06 14:22:27 +0100

    Remove the complex animation fix.

--- include/Server/Components/Fixes/fixes.hpp
+++ include/Server/Components/Fixes/fixes.hpp
@@ -8,11 +8,6 @@ struct IPlayerFixesData : public IExtension
 {
     PROVIDE_EXT_UID(FixesData_UID);
 
-    /// Has the player seen this library?  By which we mean, actually SEEN visually.
-    virtual bool seenAnimations(StringView animationLibrary) = 0;
-	//virtual bool addAnimation(StringView animationLibrary, ITimer * timer) = 0;
-	//virtual void clearAnimations() = 0;
-
 	virtual int getLastCash() const = 0;
 	virtual void setLastCash(int cash) = 0;
 };
@@ -21,9 +16,5 @@ static const UID FixesComponent_UID = UID(0xb5c615eff0329ff7);
 struct IFixesComponent : public IComponent
 {
     PROVIDE_UID(FixesComponent_UID);
-
-	virtual void reapplyAnimation(IPlayer & viewer, IPlayer & animated, AnimationData const & anim) = 0;
-	virtual void reapplyAnimation(IPlayer & viewer, IActor & animated, AnimationData const & anim) = 0;
-	virtual void clearAnimations(IPlayer & viewer, IPlayer & animated) = 0;
 };
 
