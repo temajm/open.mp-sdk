6f1c55839236fec9b227baa9ef29c085feeba7e4
Y_Less <git-athena@y-less.com>
2022-04-15 01:59:36 +0100

    Start on `PlayerGangZone`.

--- include/Server/Components/GangZones/gangzones.hpp
+++ include/Server/Components/GangZones/gangzones.hpp
@@ -12,8 +12,17 @@ struct GangZonePos {
 };
 
 /// Gangzone interace
-struct IGangZone : public IExtensible, public IIDProvider {
-    /// Check if a gangzone is shown for player
+struct IGangZoneBase : public IExtensible, public IIDProvider {
+	/// Get position of gangzone. Returns a structure of vec2 min and vec2 max coordinates
+	virtual GangZonePos getPosition() const = 0;
+
+	/// Set position of gangzone. Takes a structure of vec2 min and vec2 max coordinates
+    virtual void setPosition(const GangZonePos& position) = 0;
+};
+
+struct IGangZone : public IGangZoneBase
+{
+	/// Check if a gangzone is shown for player
     virtual bool isShownForPlayer(const IPlayer& player) const = 0;
 
     /// Check if a gangzone is flashing for player
@@ -31,12 +40,6 @@ struct IGangZone : public IExtensible, public IIDProvider {
     /// Stop flashing a gangzone for player
     virtual void stopFlashForPlayer(IPlayer& player) = 0;
 
-    /// Get position of gangzone. Returns a structure of vec2 min and vec2 max coordinates
-    virtual GangZonePos getPosition() const = 0;
-
-    /// Set position of gangzone. Takes a structure of vec2 min and vec2 max coordinates
-    virtual void setPosition(const GangZonePos& position) = 0;
-
     /// Check if specified player is within gangzone bounds (only works with IGangZonesComponent::toggleGangZoneCheck).
     virtual bool isPlayerInside(const IPlayer& player) const = 0;
 
@@ -50,9 +53,41 @@ struct IGangZone : public IExtensible, public IIDProvider {
     virtual const Colour getColorForPlayer(IPlayer& player) const = 0;
 };
 
+struct IPlayerGangZone : public IGangZoneBase
+{
+	/// Check if a gangzone is shown for player
+	virtual bool isShown() const = 0;
+
+	/// Check if a gangzone is flashing for player
+    virtual bool isFlashing() const = 0;
+
+    /// Show a gangzone for player
+    virtual void show(const Colour& colour) = 0;
+
+    /// Hide a gangzone for player
+    virtual void hide() = 0;
+
+    /// Flashing a gangzone for player
+    virtual void flash(const Colour& colour) = 0;
+
+    /// Stop flashing a gangzone for player
+    virtual void stop() = 0;
+
+    /// Check if specified player is within gangzone bounds (only works with IGangZonesComponent::toggleGangZoneCheck).
+    virtual bool isInside() const = 0;
+
+    /// get gangzone flashing color for a player
+    virtual const Colour getFlashingColor() const = 0;
+
+    /// get gangzone color for a player
+    virtual const Colour getColor() const = 0;
+};
+
 struct GangZoneEventHandler {
     virtual void onPlayerEnterGangZone(IPlayer& player, IGangZone& zone) { }
     virtual void onPlayerLeaveGangZone(IPlayer& player, IGangZone& zone) { }
+    virtual void onPlayerEnterPlayerGangZone(IPlayer& player, IGangZone& zone) { }
+    virtual void onPlayerLeavePlayerGangZone(IPlayer& player, IGangZone& zone) { }
 };
 
 static const UID GangZoneComponent_UID = UID(0xb3351d11ee8d8056);
