85f5de56ada0f48855f648441a9d9454c06e5d1f
Y_Less <git-athena@y-less.com>
2022-04-17 23:41:25 +0100

    Gang zone click events.

--- include/Server/Components/GangZones/gangzones.hpp
+++ include/Server/Components/GangZones/gangzones.hpp
@@ -61,6 +61,8 @@ struct GangZoneEventHandler {
     virtual void onPlayerLeaveGangZone(IPlayer& player, IGangZone& zone) { }
     virtual void onPlayerEnterPlayerGangZone(IPlayer& player, IGangZone& zone) { }
     virtual void onPlayerLeavePlayerGangZone(IPlayer& player, IGangZone& zone) { }
+    virtual void onPlayerClickGangZone(IPlayer& player, IGangZone& zone) { }
+    virtual void onPlayerClickPlayerGangZone(IPlayer& player, IGangZone& zone) { }
 };
 
 static const UID GangZoneComponent_UID = UID(0xb3351d11ee8d8056);
